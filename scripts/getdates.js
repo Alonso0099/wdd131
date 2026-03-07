1b31da85666b273958e7a5d80debdabc36abc87a
Alonso0099 <aalvaradovargas@byupathway.edu>
2026-03-07 12:49:37 -0700

    color changed. Notes and real text added instead "lorem". JS improved.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,18 @@
-const yearSpan = document.querySelector("#currentYear");
-yearSpan.textContent = new Date().getFullYear();
+const year = document.querySelector("#currentYear");
+year.textContent = new Date().getFullYear();
 
-const lastMod = document.querySelector("#lastModified");
+
+const lastMod =document.querySelector("#lastModified");
 lastMod.textContent = `Last Modification: ${document.lastModified}`;
+
+//This are notes for me
+/* 
+    const: create a fix variable that can not be re
+    year: its the name of the variable
+    document: represents the HTML page
+    querySelector is to target the ID that I want to apply the JS rule
+    #currentYear: is the ID in the HTML "#" is important and is case sensitive (should be the same)
+    year.textContent: text that goes in the "year" element or "#currentYear" element. "=" assign what I write to the write in th element
+    ``: whatever I write between the backticks will be the the "text" value closing with ${...}
+    ${document.lastModified}: return the string (text) with the time and date of the last modification in the document.
+*/
